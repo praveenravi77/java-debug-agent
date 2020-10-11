a45161b38355446c12b875af2d03e919454f3cf7
Praveen Ravi <praveenravi77@gmail.com>
2020-10-11 02:40:26 -0400

    logging enter and exit work, but we can only get args as an Object array, maybe use reflection to print args?

--- src/main/java/com/praveen/javaagent/AgentMain.java
+++ src/main/java/com/praveen/javaagent/AgentMain.java
@@ -6,25 +6,32 @@ import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.utility.JavaModule;
-
-import static net.bytebuddy.matcher.ElementMatchers.any;
-import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 
 public class AgentMain {
 	public static void premain(String agentOps, Instrumentation inst) {
 		System.out.println("Agent premain");
 		new AgentBuilder.Default()
 				.disableClassFormatChanges()
-//				.with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-				.with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-				.type(ElementMatchers.nameStartsWith("com.praveen.testapp")) // This handles just grabbing the classes we care about, will probably be com.rallyhealth
-				.transform(new AgentBuilder.Transformer() {
+				// BEGIN -- low key didn't read exactly what this does exactly https://stackoverflow.com/questions/35968530/retransform-classes-with-byte-buddy
+				// stack overflow thinks i need this inorder for my advice to work with hotspot jvm for transforming classes
+				.with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+				.with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
+				.with(AgentBuilder.TypeStrategy.Default.REDEFINE)
+				// END
+
+				// gets me some debug logs
+//				.with(AgentBuilder.Listener.StreamWriting.toSystemOut())
+
+				// This handles just grabbing the classes we care about, ie com.rallyhealth, THIS WORKS
+				.type(ElementMatchers.nameStartsWith("com.praveen.testapp"))
+				.transform(new AgentBuilder.Transformer.ForAdvice() {
 					@Override
 					public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule) {
-						return builder.visit(Advice.to(HelloWorldAdvice.class).on(isMethod()));
+//						return builder.method(ElementMatchers.named("toString")).intercept(FixedValue.value("bleh"));
+						// this advice does not work on toString for some reason, maybe java does fancy things to toString under the hood?
+						return builder.visit(Advice.to(LoggingAdvice.class).on(ElementMatchers.isMethod()));
 					}
 				}).installOn(inst);
 	}
@@ -34,14 +41,22 @@ public class AgentMain {
 	}
 }
 
-class HelloWorldAdvice {
-
+class LoggingAdvice {
 	@Advice.OnMethodEnter
-	static void onEnter() {
-		System.out.println("method started lol");
+	public static void enter(@Advice.Origin String origin, @Advice.AllArguments Object[] args) {
+		System.out.println("STARTED -- " + origin);
+
+		// maybe no choice but reflection here to print args?
+		// we would have to find a way to bundle in https://github.com/apache/commons-lang/blob/master/src/main/java/org/apache/commons/lang3/builder/ReflectionToStringBuilder.java
+		for (Object arg : args) {
+			System.out.println(arg);
+		}
+	}
+	@Advice.OnMethodExit(onThrowable = Throwable.class)
+	public static void exit(@Advice.Origin String origin) {
+		System.out.println("ENDED -- " + origin);
 	}
 }
-
 
 // Notes
 // Advice.AllArguments gets me all the args, not horrible
@@ -49,13 +64,14 @@ class HelloWorldAdvice {
 
 
 // Questions
-// for some reason only does retransformation for advice on the first class :shrugs:
+// Advice works as expects, just doesn't seem to work for toString
 
 
 // Flow
 // instrument play filter (maybe even make one on the fly and attach to all all controllers?) to grab debug=True and stash into ThreadLocal
 // whenever new threads are spawned, we have to check if ThreadLocal has debug=true, if so we need to copy into new thread
 // in each method with prefix of com.rallyhealth we print method name and args on enter, and print return value on exit
+// preferred instead of printing would be cool if we could collect the results and change the response to have request, response, and trace with execution tree
 
 
 
